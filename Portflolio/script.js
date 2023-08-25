21eea3cb035c2a85c2a096299b88ca6ca24219a8
Daniel Braga <danielfranciscobragadearaujo@gmail.com>
2023-08-24 23:41:43 -0300

    atualizacao

--- Portflolio/script.js
+++ Portflolio/script.js
@@ -1,6 +1,7 @@
 window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.area1', {duration: 1000});
+sr.reveal('.area2', {duration: 1000});
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
