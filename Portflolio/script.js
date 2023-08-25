6341316efab59efb516154ce5e75cb6a2cd093e6
DanielBR0612 <danielfranciscobragadearaujo@gmail.com>
2023-08-24 21:07:07 -0300

    animacao scroll

--- Portflolio/script.js
+++ Portflolio/script.js
@@ -1,3 +1,13 @@
 window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.area1', {duration: 1000});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
